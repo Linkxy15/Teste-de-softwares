654c35ee39af7addee202ea475b1aa27bcc33cb1
JOAO VITOR DE SOUSA PEREIRA <Jvitorsousa235@gmail.com>
2025-11-24 12:23:51 -0400

    Delete Pyteste/Joaovitordesousa directory

--- Pyteste/Joaovitordesousa/test_delete_post.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pytest
-
-BASE_URL = "https://jsonplaceholder.typicode.com"
-
-def test_delete_post():
-    response = requests.delete(f"{BASE_URL}/posts/1")
-    assert response.status_code == 200
